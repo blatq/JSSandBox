c34649d317969b1c81e5d0c5aad452c12a3f6509
blatq <78491203+blatq@users.noreply.github.com>
2022-05-29 21:48:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Log to console
+console.log('Hello World');
+console.log(120);
+console.log(true);
+var greating = 'Hello';
+console.log(greating);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
+/*
+ multi
+ lane
+ coments
+*/
